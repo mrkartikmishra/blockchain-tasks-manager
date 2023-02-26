669326a01ba0b0d42238289fa2ee8da0c2b5740a
Kartik Mishra <kartikmishra.km44@gmail.com>
2023-02-26 12:51:56 +0530

    deployment script added

--- server/scripts/deploy.js
+++ server/scripts/deploy.js
@@ -1,17 +1,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const TaskManagerContract = await hre.ethers.getContractFactory("TaskManagerContract");
+  const taskManagerContract = await TaskManagerContract.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.01");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await taskManagerContract.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp deployed to ${lock.address}`
+    `TaskManagerContract deployed to:: ${taskManagerContract.address}`
   );
 }
 main().catch((error) => {
